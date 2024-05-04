3c511b1152a312bdccc73bc4e88b0b47396eb629
JhonFajardoRodas <jj.fajardo17@hotmail.com>
2024-05-04 19:52:37 +0200

    Modificaciones del notebook de visualización y la app streamlit

---
+++ notebooks/Modelado/Visualización/app_visualizacion.py
@@ -0,0 +1,104 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+# Se realiza la lectura de los datos
+df = pd.read_csv("../../../data/final/datos_finales.csv", sep=";")
+
+# Título del dashboard
+st.write("# 13MBID - Visualización de datos")
+st.write("## Panel de visualización generado sobre los datos de créditos y tarjetas emitidas a clientes de la entidad")
+st.write("#### Persona/s: ................")
+st.write("----")
+
+# Gráficos
+st.write("### Caracterización de los créditos otorgados")
+
+# Se tienen que agregar las definiciones de gráficos desde la libreta
+creditos_x_objetivo = px.histogram(df, x='objetivo_credito', 
+                                   title='Conteo de créditos por objetivo')
+creditos_x_objetivo.update_layout(xaxis_title='Objetivo del crédito', yaxis_title='Cantidad')
+
+# Se realiza la "impresión" del gráfico en el dashboard
+st.plotly_chart(creditos_x_objetivo)
+
+
+# Histograma de los importes de créditos otorgados
+histograma_importes = px.histogram(df, x='importe_solicitado', nbins=10, title='Importes solicitados en créditos')
+histograma_importes.update_layout(xaxis_title='Importe solicitado', yaxis_title='Cantidad')
+
+st.plotly_chart(histograma_importes)
+
+
+#gráfico del Dtale
+
+# remove any pre-existing indices for ease of use in the D-Tale code, but this is not required
+df = df.reset_index().drop('index', axis=1, errors='ignore')
+df.columns = [str(c) for c in df.columns]  # update columns to strings in case they are numbers
+
+s = df[~pd.isnull(df['limite_credito_tc'])]['limite_credito_tc']
+chart = pd.value_counts(s).to_frame(name='data')
+chart['percent'] = (chart['data'] / chart['data'].sum()) * 100
+chart.index.name = 'labels'
+chart = chart.reset_index().sort_values(['data', 'labels'], ascending=[False, True])
+chart = chart[:100]
+charts = [go.Bar(x=chart['labels'].values, y=chart['data'].values, name='Frequency')]
+figure = go.Figure(data=charts, layout=go.Layout({
+    'barmode': 'group',
+    'legend': {'orientation': 'h'},
+    'title': {'text': 'limite_credito_tc Value Counts'},
+    'xaxis': {'title': {'text': 'limite_credito_tc'}},
+    'yaxis': {'title': {'text': 'Frequency'}}
+}))
+
+# If you're having trouble viewing your chart in your notebook try passing your 'chart' into this snippet:
+#
+# from plotly.offline import iplot, init_notebook_mode
+#
+# init_notebook_mode(connected=True)
+# for chart in charts:
+#     chart.pop('id', None) # for some reason iplot does not like 'id'
+# iplot(figure)
+
+st.plotly_chart(figure)
+
+
+
+# Conteo de ocurrencias por caso
+falta_pago_counts = df['falta_pago'].value_counts()
+
+# Create a Pie chart
+fig = go.Figure(data=[go.Pie(labels=falta_pago_counts.index, values=falta_pago_counts)])
+fig.update_layout(title_text='Distribución de créditos en función de registro de mora')
+st.plotly_chart(fig)
+
+# Filtros
+
+#option = st.selectbox(
+ #   'Qué tipo de crédito desea filtrar?',
+  #   df['objetivo_credito'].unique())
+
+#df_filtrado = df[df['objetivo_credito'] == option]
+
+#st.write(f"Tipo de crédito seleccionado: {option}")
+
+#if st.checkbox('Mostrar créditos finalizados?', value=True):
+
+    # Conteo de ocurrencias por estado
+ #   estado_credito_counts = df_filtrado['estado_credito_N'].value_counts()
+
+    # Gráfico de torta de estos valores
+  #  fig = go.Figure(data=[go.Pie(labels=estado_credito_counts.index, values=estado_credito_counts)])
+   # fig.update_layout(title_text='Distribución de créditos por estado registrado')
+#else:
+ #   df_filtrado = df_filtrado[df_filtrado['estado_credito_N'] == 'P']
+    # Conteo de ocurrencias por caso
+  #  falta_pago_counts = df_filtrado['falta_pago'].value_counts()
+
+    # Create a Pie chart
+    #fig = go.Figure(data=[go.Pie(labels=falta_pago_counts.index, values=falta_pago_counts)])
+    #fig.update_layout(title_text='Distribución de créditos en función de registro de mora')
+
+#st.write(f"Cantidad de créditos con estas condiciones: {df_filtrado.shape[0]}")
+#st.plotly_chart(fig)
